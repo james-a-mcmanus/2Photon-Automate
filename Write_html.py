c854bc817cc79ef9a246fc1c6428001f633b4b34
KeivanR <keivan.razban@gmail.com>
2021-04-27 19:33:18 +0100

    Add screenshot on html file

--- Write_html.py
+++ Write_html.py
@@ -1,10 +1,14 @@
-def update(text,size):
-    f = open('C:\\Users\\lvbt\\Documents\\index.html','w')
-
+import Experiment_parameters as exp
+import os
+def update(text,size,path = exp.path):
+    os.system('python C:\\Users\\lvbt\\Documents\\GitHub\\2Photon-Automate\\Screenshot.py')
+    f = open(path+'index.html','w')
     message = """<html>
     <head></head>
     <body> <p style='font-size:"""+str(size)+"""px'>""" + text + """</p> </body>
+    <img src='screenshot.png' alt="Current screen" width = '1000' height = '600'>  
     </html>"""
 
     f.write(message)
     f.close()
+#update('lol',33)
